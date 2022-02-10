7b075012964571cb259e961248619f71ee5dba4f
Heyden <nhm6947@gmail.com>
2022-02-11 05:57:53 +0900

    Feat: 업로드 페이지 데이터베이스에 저장

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {Typography, Button, Form, Input} from 'antd';
 import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
 const { TextArea } = Input;
 
 const Continents = [
@@ -13,7 +14,7 @@ const Continents = [
     { key: 7, value: "Antarctica"},
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
 
     const [Title, setTitle] = useState("")
     const [Description, setDescription] = useState("")
@@ -41,7 +42,35 @@ function UploadProductPage() {
         setImages(newImages)
     }
 
+    const submitHandler = (event) => {
+        // event.preventDefault();
 
+        if(!Title || !Description || !Price || !Continent || !Images) {
+            return alert("모든 값을 넣어야 합니다.")
+        }
+
+        // 서버에 채운 값들을 request로 보낸다.
+
+        const body = {
+            // 로그인된 사람의 ID
+            writer: props.user.userData._id,
+            title: Title,
+            description: Description,
+            price: Price,
+            images: Images,
+            continents: Continent
+        }
+
+        Axios.post("/api/product", body)
+            .then(response => {
+                if(response.data.success) {
+                    alert('상품 업로드에 성공 했습니다.')
+                    props.history.push('/')
+                } else {
+                    alert('상품 업로드에 실패 했습니다.')
+                }
+            })
+    }
 
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
@@ -49,11 +78,8 @@ function UploadProductPage() {
                 <h2>여행 상품 업로드</h2>
             </div>
 
-
-            <Form>
-
+            <Form onSubmit={submitHandler}>
                 {/* DropZone */}
-                
                 <FileUpload refreshFunction={updateImages} />
                 
                 <br />
@@ -79,8 +105,7 @@ function UploadProductPage() {
                 </select>
                 <br />
                 <br />
-                <Button>
-
+                <Button type="submit" onClick={submitHandler}>
                     확인
                 </Button>
             </Form>
